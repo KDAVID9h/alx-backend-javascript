7b4428f9462daab075f9d7a497842a6b0be8c54d
KDAVID9h <hognonkossidavid@gmail.com>
2024-10-03 05:12:25 +0000

    Ajout de 0x01-ES6_promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+const Photo = uploadPhoto();
+const User = createUser();
+export default function handleProfileSignup() {
+  return Promise
+    .all([Photo, User])
+    .then((results) => {
+      const msg = `${results[0].body} ${results[1].firstName} ${results[1].lastName}`;
+      console.log(msg);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const val = mathFunction();
+    queue.push(val);
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
